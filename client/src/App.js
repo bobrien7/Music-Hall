10fa8207d66d1e339f16c1bba117cd71c884b422
L Cai <lizbcai@gmail.com>
2023-01-26 15:30:55 -0800

    Test Wikipedia API short extract retrieval and display on site

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,41 @@ const config = require('./config.json');
 
 function App() {
 
-  const [testString, setTestString] = useState("");
+  const [testString, setTestString] = useState("the backend is not connected!");
+  const [titleString, setTitleString] = useState("");
+  const [wikiString, setWikiString] = useState("");
 
   useEffect(() => {
     fetch(`http://${config.server_host}:${config.server_port}/test`)
       .then(res => res.text())
       .then(resJson => {setTestString(resJson)});
-  }, []);
+
+    let search = "Franz Ferdinand music";
+
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srsearch=${search}`)
+      .then(res => res.json())
+      .then(resJson => setTitleString(resJson["query"]["search"][0]["title"]));
+
+    fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=extracts&titles=${titleString}&formatversion=2&exintro=1`)
+      .then(res => res.json())
+      .then(resJson => {
+        let string = new DOMParser().parseFromString(resJson["query"]["pages"][0]["extract"], "text/html");
+        setWikiString(string.firstChild.innerText);
+      });
+  }, [titleString]);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          test
+          {titleString}
         </p>
-        {testString}
+        <p>
+          {testString}
+        </p>
       </header>
-    </div>
+      {wikiString}
+      </div>
   );
 }
 
