f6785b04171385f9a62a04dcf96b042b981fba33
L Cai <lizbcai@gmail.com>
2023-02-05 15:41:29 -0800

    Test display album cover and play Spotify preview on page w/ info from API

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
       .then(res => res.text())
       .then(resJson => {setTestString(resJson)});
 
-    let search = "Franz Ferdinand music";
+    let search = "guns n roses";
 
     fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srsearch=${search}`)
       .then(res => res.json())
@@ -38,6 +38,15 @@ function App() {
         </p>
       </header>
       {wikiString}
+
+      <p>
+      <img src="https://i.scdn.co/image/ab67616d00001e0221ebf49b3292c3f0f575f0f5" alt="album cover" />
+      </p>
+
+      <p>
+      <audio controls src="https://p.scdn.co/mp3-preview/80a49eba7f6517d4f1364e5b0a96d5dd08cff4ef?cid=4253f1c121cd47208ee35324d5b090b2"></audio>
+      </p>
+
       </div>
   );
 }
